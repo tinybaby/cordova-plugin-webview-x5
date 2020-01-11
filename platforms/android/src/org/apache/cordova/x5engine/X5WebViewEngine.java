a0ccfd17db5b70ffd39460449393bbddd3781469
CNST <guard.st@gmail.com>
2020-01-11 16:52:41 +0800

    添加Ionic4支持

--- platforms/android/src/org/apache/cordova/x5engine/X5WebViewEngine.java
+++ platforms/android/src/org/apache/cordova/x5engine/X5WebViewEngine.java
@@ -21,19 +21,29 @@ package org.apache.cordova.x5engine;
 
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
 import android.view.View;
 import android.webkit.ValueCallback;
 
+import com.tencent.smtt.export.external.interfaces.WebResourceRequest;
+import com.tencent.smtt.export.external.interfaces.WebResourceResponse;
 import com.tencent.smtt.sdk.WebSettings;
 import com.tencent.smtt.sdk.WebSettings.LayoutAlgorithm;
 import com.tencent.smtt.sdk.WebView;
 
+import org.apache.cordova.ConfigXmlParser;
 import org.apache.cordova.CordovaBridge;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPreferences;
@@ -72,6 +82,15 @@ public class X5WebViewEngine implements CordovaWebViewEngine {
     protected NativeToJsMessageQueue nativeToJsMessageQueue;
     private BroadcastReceiver receiver;
 
+    public static final String WEBVIEW_PREFS_NAME = "WebViewSettings";
+    public static final String CDV_SERVER_PATH = "serverBasePath";
+
+    private WebViewLocalServer localServer;
+    private String CDV_LOCAL_SERVER;
+    private String scheme;
+    private static final String LAST_BINARY_VERSION_CODE = "lastBinaryVersionCode";
+    private static final String LAST_BINARY_VERSION_NAME = "lastBinaryVersionName";
+
     /** Used when created via reflection. */
     public X5WebViewEngine(Context context, CordovaPreferences preferences) {
         this(new X5WebView(context), preferences);
@@ -104,6 +123,19 @@ public class X5WebViewEngine implements CordovaWebViewEngine {
         this.resourceApi = resourceApi;
         this.pluginManager = pluginManager;
         this.nativeToJsMessageQueue = nativeToJsMessageQueue;
+
+        ConfigXmlParser parser = new ConfigXmlParser();
+        parser.parse(cordova.getActivity());
+
+        String hostname = preferences.getString("Hostname", "localhost");
+        scheme = preferences.getString("Scheme", "http");
+        CDV_LOCAL_SERVER = scheme + "://" + hostname;
+
+        localServer = new WebViewLocalServer(cordova.getActivity(), hostname, true, parser, scheme);
+        localServer.hostAssets("www");
+
+        webView.setWebViewClient(new ServerClient(this, parser));
+
         webView.init(this, cordova);
 
         initWebViewSettings();
@@ -122,6 +154,92 @@ public class X5WebViewEngine implements CordovaWebViewEngine {
             nativeToJsMessageQueue.addBridgeMode(new NativeToJsMessageQueue.EvalBridgeMode(this, cordova));
         bridge = new CordovaBridge(pluginManager, nativeToJsMessageQueue);
         exposeJsInterface(webView, bridge);
+        SharedPreferences prefs = cordova.getActivity().getApplicationContext().getSharedPreferences(WEBVIEW_PREFS_NAME, Activity.MODE_PRIVATE);
+        String path = prefs.getString(CDV_SERVER_PATH, null);
+        if (!isDeployDisabled() && !isNewBinary() && path != null && !path.isEmpty()) {
+            setServerBasePath(path);
+        }
+    }
+    private boolean isNewBinary() {
+        String versionCode = "";
+        String versionName = "";
+        SharedPreferences prefs = cordova.getActivity().getApplicationContext().getSharedPreferences(WEBVIEW_PREFS_NAME, Activity.MODE_PRIVATE);
+        String lastVersionCode = prefs.getString(LAST_BINARY_VERSION_CODE, null);
+        String lastVersionName = prefs.getString(LAST_BINARY_VERSION_NAME, null);
+
+        try {
+            PackageInfo pInfo = this.cordova.getActivity().getPackageManager().getPackageInfo(this.cordova.getActivity().getPackageName(), 0);
+            versionCode = Integer.toString(pInfo.versionCode);
+            versionName = pInfo.versionName;
+        } catch(Exception ex) {
+            Log.e(TAG, "Unable to get package info", ex);
+        }
+
+        if (!versionCode.equals(lastVersionCode) || !versionName.equals(lastVersionName)) {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString(LAST_BINARY_VERSION_CODE, versionCode);
+            editor.putString(LAST_BINARY_VERSION_NAME, versionName);
+            editor.putString(CDV_SERVER_PATH, "");
+            editor.apply();
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isDeployDisabled() {
+        return preferences.getBoolean("DisableDeploy", false);
+    }
+    private class ServerClient extends X5WebViewClient {
+        private ConfigXmlParser parser;
+
+        public ServerClient(X5WebViewEngine parentEngine, ConfigXmlParser parser) {
+            super(parentEngine);
+            this.parser = parser;
+        }
+
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        @Override
+        public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+            return localServer.shouldInterceptRequest(request.getUrl(), request);
+        }
+
+        @TargetApi(Build.VERSION_CODES.KITKAT)
+        @Override
+        public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
+            return localServer.shouldInterceptRequest(Uri.parse(url), null);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            String launchUrl = parser.getLaunchUrl();
+            if (!launchUrl.contains(WebViewLocalServer.httpsScheme) && !launchUrl.contains(WebViewLocalServer.httpScheme) && url.equals(launchUrl)) {
+                view.stopLoading();
+                // When using a custom scheme the app won't load if server start url doesn't end in /
+                String startUrl = CDV_LOCAL_SERVER;
+                if (!scheme.equalsIgnoreCase(WebViewLocalServer.httpsScheme) && !scheme.equalsIgnoreCase(WebViewLocalServer.httpScheme)) {
+                    startUrl += "/";
+                }
+                view.loadUrl(startUrl);
+            }
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            view.loadUrl("javascript:(function() { " +
+                    "window.WEBVIEW_SERVER_URL = '" + CDV_LOCAL_SERVER + "';" +
+                    "})()");
+        }
+    }
+
+    public void setServerBasePath(String path) {
+        localServer.hostFiles(path);
+        webView.loadUrl(CDV_LOCAL_SERVER);
+    }
+
+    public String getServerBasePath() {
+        return this.localServer.getBasePath();
     }
 
     @Override
